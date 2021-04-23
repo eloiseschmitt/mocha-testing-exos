45463139d8506d5412b77eba5fbca15db741e5ee
Eloise Schmitt <eschmitt@atixnet.fr>
2021-04-23 15:16:09 +0200

    séparation des questions et réponses, export des fonctions de réponse

--- test.js
+++ test.js
@@ -1,36 +1,4 @@
-const chunk = (array, size) => {
-  const chunks = [];
-
-  for (let item of array) {
-    const lastChunk = chunks[chunks.length - 1];
-    if (!lastChunk || lastChunk.length === size) chunks.push([item]);
-    else lastChunk.push(item);
-  }
-
-  return chunks;
-};
-
-const _chunk = (array, size) => {
-  const chunks = [];
-  let index = 0;
-
-  while (index < array.length) {
-    chunks.push(array.slice(index, index + size));
-    index += size;
-  }
-
-  return chunks;
-};
-
-const __chunk = (array, size) => {
-  const chunks = [];
-
-  for (let i = 0; i < array.length; i += size) {
-    chunks.push(array.slice(i, i + size));
-  }
-
-  return chunks;
-};
+const { chunk, _chunk, __chunk} = require('./responses')
 
 const chai = require('chai')
 
